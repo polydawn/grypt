dac3c019404c1b129e1de780e6b732708b339070
Hank Donnay <hdonnay@gmail.com>
2014-03-28 17:26:38 -0500

    reset the plaintext Reader

--- benchutil_test.go
+++ benchutil_test.go
@@ -3,10 +3,9 @@ package main
 import (
 	"bytes"
 	"crypto/rand"
+	"io/ioutil"
 	"testing"
 )
-
-var x int
 
 func encBench(b *testing.B, s Scheme, sz int) {
 	k, err := NewKey(rand.Reader, s)
@@ -14,15 +13,12 @@ func encBench(b *testing.B, s Scheme, sz int) {
 		b.Fatal(err)
 	}
 	plaintext := bytes.NewReader(mkRand(sz))
-	o := new(bytes.Buffer)
-	o.Grow(sz)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		if err := Encrypt(plaintext, o, k); err != nil {
+		if err := Encrypt(plaintext, ioutil.Discard, k); err != nil {
 			b.Fatal(err)
 		}
-		x = o.Len()
-		o.Reset()
+		plaintext.Seek(0, 0)
 	}
 	return
 }
@@ -32,22 +28,18 @@ func decBench(b *testing.B, s Scheme, sz int) {
 	if err != nil {
 		b.Fatal(err)
 	}
-	plaintext := bytes.NewBuffer(mkRand(sz))
 	buf := new(bytes.Buffer)
 	buf.Grow(sz)
-	if err := Encrypt(plaintext, buf, k); err != nil {
-		panic(err)
+	if err := Encrypt(bytes.NewReader(mkRand(sz)), buf, k); err != nil {
+		b.Fatal(err)
 	}
 	r := bytes.NewReader(buf.Bytes())
-	o := new(bytes.Buffer)
-	o.Grow(sz)
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		if err := Decrypt(r, o, k); err != nil {
+		if err := Decrypt(r, ioutil.Discard, k); err != nil {
 			b.Fatal(err)
 		}
 		r.Seek(0, 0)
-		o.Reset()
 	}
 	return
 }
