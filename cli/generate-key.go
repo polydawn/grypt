38f9398ce9b8e0352bb5341e303ad1aa15f1a020
Eric Myhre <hash@exultant.us>
2014-09-09 22:04:58 -0500

    functionality to write a key, as previously.  currently hardcoded an obviously wrong filename; going to build more of a schema of defaults revolving around the .git directory to try to keep it out of sight and out of mind.

--- cli/generate-key.go
+++ cli/generate-key.go
@@ -1,9 +1,18 @@
 package cli
 
 import (
+	"crypto/rand"
+	"fmt"
 	grypt "polydawn.net/grypt"
 )
 
 func GenerateKey(keyring string, random bool, password []byte, encryptionScheme grypt.Scheme) {
-
+	k, err := grypt.NewKey(rand.Reader, encryptionScheme)
+	if err != nil {
+		panic(fmt.Errorf("failure generating key: %v", err))
+	}
+	err = grypt.WriteKey("hardcoded_fixme", k)
+	if err != nil {
+		panic(fmt.Errorf("failure saving key: %v", err))
+	}
 }
