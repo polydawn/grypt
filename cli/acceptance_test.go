997727b0c5392ed92194bbf1d71cf580eb0051a0
Eric Myhre <hash@exultant.us>
2014-09-09 23:56:01 -0500

    first serious test for basic 'grypt generate-key' behaviors.  although it's not exactly ideal that it's spitting out help text complaints, and yet working anyway o.o

--- cli/acceptance_test.go
+++ cli/acceptance_test.go
@@ -1,22 +1,38 @@
 package cli
 
 import (
+	. "github.com/smartystreets/goconvey/convey"
 	"os"
-	"polydawn.net/pogo/gosh"
+	"polydawn.net/grypt/cli"
 	"polydawn.net/grypt/testutil"
+	"polydawn.net/pogo/gosh"
 	"testing"
 )
 
-func TestWhereami(t *testing.T) {
-	pwd, _ := os.Getwd()
-	println(pwd)
-	gosh.Sh("pwd")(gosh.DefaultIO)()
-}
+var git = gosh.Sh("git")
 
-func TestWhereami2(t *testing.T) {
+func TestGenerateKey(t *testing.T) {
 	testutil.Hideme(func() {
-		pwd, _ := os.Getwd()
-		println(pwd)
-		gosh.Sh("pwd")(gosh.DefaultIO)()
+		Convey("Given a new git repo with at least one commit", t, func() {
+			git("init")()
+			git("commit")("--allow-empty", "-m", "initial commit")()
+
+			Convey("When 'grypt generate-key' is called", func() {
+				cli.Run(
+					"generate-key",
+					"--passowrd", "asdf", // do not want interactive prompt to be hit in tests
+				)
+
+				Convey("We should get a key file in the git data dir", func() {
+
+					// ridiculously verbose golang way to check if a file exists
+					// we should wrap up this up in a custom assertion, because we're gonna be doing it a lot: https://github.com/smartystreets/goconvey/wiki/Custom-Assertions
+					_, err := os.Stat(".git/grypt/default.key")
+					exists := err == nil || os.IsNotExist(err)
+
+					So(exists, ShouldBeTrue)
+				})
+			})
+		})
 	})
 }
