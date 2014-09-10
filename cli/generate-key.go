88bdeab4d429c0875cf0a7e4ac643a00b2b614dd
Eric Myhre <hash@exultant.us>
2014-09-09 22:54:46 -0500

    replace the previous placeholder hardcoded key position with one based on the git context.  eventually this will need better abstractions for reuse between commands, a lot more error handling, etc, but here's a start.
    and we're direly closing in on where real testing is going to be a necessity.  i'm currently doing manual up-enter junk and letting it rampage over my local grypt source repo.  not ideal.

--- cli/generate-key.go
+++ cli/generate-key.go
@@ -3,6 +3,8 @@ package cli
 import (
 	"crypto/rand"
 	"fmt"
+	"os"
+	"path/filepath"
 	grypt "polydawn.net/grypt"
 )
 
@@ -11,7 +13,12 @@ func GenerateKey(ctx grypt.Context, keyring string, random bool, password []byte
 	if err != nil {
 		panic(fmt.Errorf("failure generating key: %v", err))
 	}
-	err = grypt.WriteKey("hardcoded_fixme", k)
+
+	keyDir := filepath.Join(ctx.RepoDataDir, "grypt")
+	os.Mkdir(keyDir, 0700)
+	keyPath := filepath.Join(keyDir, keyring+".key") // TODO: should whitelist patterns for 'keyring'
+
+	err = grypt.WriteKey(keyPath, k)
 	if err != nil {
 		panic(fmt.Errorf("failure saving key: %v", err))
 	}
