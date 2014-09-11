8836223fef3f10b3e2a5bc6adf6e8b16bcf687d3
Eric Myhre <hash@exultant.us>
2014-09-10 21:46:35 -0500

    future work note

--- gitutil/gitutil.go
+++ gitutil/gitutil.go
@@ -13,4 +13,5 @@ func PutGitFilterConfig(ctx grypt.Context) {
 	git("config", "filter.grypt.smudge", fmt.Sprintf("%s git-smudge", exeName))()
 	git("config", "filter.grypt.clean", fmt.Sprintf("%s git-clean", exeName))()
 	git("config", "diff.grypt.textconv", fmt.Sprintf("%s git-textconv", exeName))()
+	// TODO: experiment with the 'required' config parameter, see `man gitattributues`.  making ourselves 'required' but exit 0 when a key is absent could give us desirable behavior like erroring when the command path isn't working.
 }
