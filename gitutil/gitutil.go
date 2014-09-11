fba39b0f7b954b9d787fa91b139037e60dd0b559
Eric Myhre <hash@exultant.us>
2014-09-10 21:48:52 -0500

    percent-f can be used to get the relevant file as an argument, let's play with that

--- gitutil/gitutil.go
+++ gitutil/gitutil.go
@@ -10,8 +10,8 @@ var git = gosh.Sh("git")
 const exeName = "grypt" // not sure of the best way to make this more general.  taking '$0' isn't necessarily the most stable idea either.
 
 func PutGitFilterConfig(ctx grypt.Context) {
-	git("config", "filter.grypt.smudge", fmt.Sprintf("%s git-smudge", exeName))()
-	git("config", "filter.grypt.clean", fmt.Sprintf("%s git-clean", exeName))()
-	git("config", "diff.grypt.textconv", fmt.Sprintf("%s git-textconv", exeName))()
+	git("config", "filter.grypt.smudge", fmt.Sprintf("%s git-smudge %%f", exeName))()
+	git("config", "filter.grypt.clean", fmt.Sprintf("%s git-clean %%f", exeName))()
+	git("config", "diff.grypt.textconv", fmt.Sprintf("%s git-textconv %%f", exeName))()
 	// TODO: experiment with the 'required' config parameter, see `man gitattributues`.  making ourselves 'required' but exit 0 when a key is absent could give us desirable behavior like erroring when the command path isn't working.
 }
