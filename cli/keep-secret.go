a989d6d8cd0525a7ac8a6ab3d1a9db315c4820f0
Eric Myhre <hash@exultant.us>
2014-09-12 00:06:51 -0500

    add a check that files specified to the KeepSecret command actually exist.
    ... actually, not 100% sure this is a correct behavior, but let's run with it for now.  maybe in the future we add a "--ignore-missing" option in case you do want to tell grypt about files that should be secret before they actually exist.

--- cli/keep-secret.go
+++ cli/keep-secret.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"os"
 	grypt "polydawn.net/grypt"
 	"polydawn.net/grypt/gitutil"
 	"polydawn.net/pogo/gosh"
@@ -9,11 +10,27 @@ import (
 var git = gosh.Sh("git")
 
 func KeepSecret(ctx grypt.Context, keyring string, files []string) {
+	// check up front that all the secret files exist
+	// this is racey with other checks later, but those later checks are done by git and come back to use as undifferentiated exit codes, so there's only so much we can do here.
+	// exit if any of the secret files don't exist
+	for _, file := range files {
+		_, err := os.Stat(file)
+		if err != nil {
+			panic(err) // TODO: should probably exit politely with a well-known status code for file-not-found.  but needs infra: we can't literally os.Exit here because tests.
+		}
+	}
+
+	// TODO: check if any of these files are staged or committed already, because that probably means the cleartext is in git history
+	// we should abort and warn about that situation because it's very much not what you wanted.
+
+	// write new '.gitattributes' config file
 	gitattrs := gitutil.ReadRepoGitAttribs(ctx)
 	for _, file := range files {
 		gitattrs.PutGryptEntry(file)
 	}
 	gitattrs.SaveRepoGitAttribs(ctx)
+
+	// stage it all
 	git(gosh.DefaultIO)("add", "--", ".gitattributes")()
 	for _, file := range files {
 		git(gosh.DefaultIO)("add", "--", file)(gosh.Opts{OkExit: []int{0, 128}})()
