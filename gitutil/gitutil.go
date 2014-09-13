7c56bc97fccf9b523c22c1ad7d61a949262c8625
Eric Myhre <hash@exultant.us>
2014-09-13 15:24:34 -0500

    fiddled around with the gitconfig a little more; keeping comments.
    notice that PutGitFilterConfig isn't actually being called anywhere yet.... yeah, about that.  right now if you, say, put it at the front of KeepSecret, you'll get wicked error messages from git about commands not found.
    
    we need to figure out how we're going to deal with tests where our execs of git need to exec grypt again.  i'm loathe to teach the test code how to drive the whole compiler, but i don't really see a lot of alternatives.

--- gitutil/gitutil.go
+++ gitutil/gitutil.go
@@ -20,8 +20,10 @@ const exeName = "grypt" // not sure of the best way to make this more general.  
 func PutGitFilterConfig(ctx grypt.Context) {
 	git("config", "filter.grypt.smudge", fmt.Sprintf("%s git-smudge %%f", exeName))()
 	git("config", "filter.grypt.clean", fmt.Sprintf("%s git-clean %%f", exeName))()
-	git("config", "diff.grypt.textconv", fmt.Sprintf("%s git-textconv %%f", exeName))()
 	// TODO: experiment with the 'required' config parameter, see `man gitattributues`.  making ourselves 'required' but exit 0 when a key is absent could give us desirable behavior like erroring when the command path isn't working.
+	// if you have these filter configs in place but not required and the grypt command missing, you get very ugly error messages from git anyway.
+	//git("config", "filter.grypt.required", "true")()
+	git("config", "diff.grypt.textconv", fmt.Sprintf("%s git-textconv %%f", exeName))()
 }
 
 /*
