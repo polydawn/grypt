a6ab852ffffdbf03624fb70d8054ac51c50f959f
Eric Myhre <hash@exultant.us>
2014-09-13 13:35:11 -0500

    more testing for the git inspections utils -- making sure the staged changes thing still works like I think it does when the diffs are on top of existing files.

--- gitutil/inspections_test.go
+++ gitutil/inspections_test.go
@@ -40,7 +40,56 @@ func TestInspectingStagedObjects(t *testing.T) {
 		}),
 	)
 
-	// TODO also test that we get the right (full) contents when staging a diff on top of existing file
+	Convey("Given a repo with existing commits", t,
+		testutil.WithTmpdir(func() {
+			git = gosh.Sh("git")
+			git("init")()
+			git("commit")("--allow-empty", "-m", "initial commit")()
+
+			err := ioutil.WriteFile("alpha", []byte("111"), 0644)
+			So(err, ShouldBeNil)
+			err = ioutil.WriteFile("beta", []byte("222"), 0644)
+			So(err, ShouldBeNil)
+
+			git("add", "--", "alpha", "beta")()
+			git("commit")("-m", "actual content commit")()
+
+			Convey("ListStagedFiles should be empty", func() {
+				stagedObjectIds := ListStagedFiles()
+				So(len(stagedObjectIds), ShouldEqual, 0)
+			})
+
+			Convey("When additional changes are made to an existing file but not staged", func() {
+				err := ioutil.WriteFile("alpha", []byte("333"), 0644)
+				So(err, ShouldBeNil)
+
+				Convey("ListStagedFiles should be empty", func() {
+					stagedObjectIds := ListStagedFiles()
+					So(len(stagedObjectIds), ShouldEqual, 0)
+				})
+			})
+
+			Convey("When additional changes are staged to an existing file", func() {
+				err := ioutil.WriteFile("alpha", []byte("333"), 0644)
+				So(err, ShouldBeNil)
+
+				git("add", "--", "alpha", "beta")()
+
+				Convey("ListStagedFiles should return the right filenames", func() {
+					stagedObjectIds := ListStagedFiles()
+					So(len(stagedObjectIds), ShouldEqual, 1)
+					_, ok := stagedObjectIds["alpha"]
+					So(ok, ShouldBeTrue)
+				})
+
+				Convey("ShowStagedFileContents should return the raw contents", func() {
+					stagedContents := ListStagedFileContents()
+					So(len(stagedContents), ShouldEqual, 1)
+					So(stagedContents["alpha"], ShouldResemble, []byte("333"))
+				})
+			})
+		}),
+	)
 
 	// TODO also test repo initialization?  though if git makes that hard, i'm very willing to give that up as a feature.  most of git itself completely sucks on a zero-commit repo.
 }
