268008592e5c63b9f9384b28243302864dbb9f6e
Eric Myhre <hash@exultant.us>
2014-09-13 20:44:24 -0500

    new package.  grypt's content storage doesn't have much in the way of structure yet, but we should add some for both headers, futureproofability of the serial form, and just plain human readability.  so this package will hold that stuff.

---
+++ vault/doc.go
@@ -0,0 +1,8 @@
+/*
+	Specifications for content storage formats.
+
+	Ciphertext produced by grypt contains some headers,
+	both for human-readable hints in case of system failure,
+	as well as for grypt to read before unwinding the crypto.
+*/
+package vault
