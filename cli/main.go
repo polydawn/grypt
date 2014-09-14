62a724a364201e0863d5b4f40b8ca4017ce852b5
Hank Donnay <hdonnay@gmail.com>
2014-09-13 20:45:20 -0500

    go-go go hdkf

--- cli/main.go
+++ cli/main.go
@@ -1,10 +1,11 @@
 package cli
 
 import (
-	"code.google.com/p/go.crypto/ssh/terminal"
 	"fmt"
-	"github.com/codegangsta/cli"
 	"os"
+
+	"code.google.com/p/go.crypto/ssh/terminal"
+	"github.com/codegangsta/cli"
 	grypt "polydawn.net/grypt"
 )
 
@@ -47,7 +48,7 @@ func Run(args ...string) {
 				},
 				cli.StringFlag{
 					Name:   "password",
-					Usage:  "provide a password for generating the key.  if not provided, it will be prompted for interactively -- and this is the preferred mechanism, as this prevents your password from showing up trivially to others on the same machine in the output of commands like `ps`!  in grypt's default behavior, this password will be used to derive the symmetric key which will then encrypt your secrets; if combined with the '--random-key' option, this will instead cause the random key to be itself protected in another layer of symmetric encryption with a password-derived key (much like adding a password to ssh keys).",
+					Usage:  "provide a password for generating the key.  if not provided, it will be prompted for interactively -- and this is the preferred mechanism, as this prevents your password from showing up trivially to others on the same machine in the output of commands like `ps`!  in grypt's default behavior, this password will be used to derive the symmetric key which will then encrypt your secrets.",
 					EnvVar: "GRYPT_PASSWORD",
 				},
 				cli.StringFlag{
