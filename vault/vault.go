da0032d12160c00d655c14e2e4680ba4ca6c4734
Eric Myhre <hash@exultant.us>
2014-09-14 13:39:23 -0500

    broaden headers out to key-value pairs.  driving for a futureproof behavior much like that of specs like protobuf (if we don't recognize a value, we'll damn well keep it on the assumption that some other version of us out there knew what it was doing).

--- vault/vault.go
+++ vault/vault.go
@@ -10,8 +10,49 @@ import (
 	grypt "polydawn.net/grypt"
 )
 
+/*
+	Grypt content vault format headers are simple string key-value pairs.
+	They roughly follow the outline of git commit message suffixes:
+
+	- keys should be formatted as "Title-Case-Phrases:" terminated with a colon, then whitespace.
+	  - keys may not contain whitespace or colon characters.
+	  - keys are case sensitive.
+	  - it is strongly recommended that keys only contain alphanum and dashes.
+	- values follow keys, and must be a single line.
+
+	Keys and values are both interpretted as UTF-8 sequences.  There is no strict length on either keys or values.
+
+	Certain keys are well-known.  Those are declared in consts in this package.
+	Other keys will be passed through when grypt decrypts, alters cleartext payload, and recrypts, even if grypt does not recognize them as well-known keys.
+*/
+type Headers map[string]string
+
+// ^ note: goddamnit, ordered maps again.  it would certainly be charming to put well-known keys first, and maintain orders of others so diffs aren't randomly shitty.  as it is we'll have to sort and call it a day.
+
+const (
+	/*
+		Names the version of grypt that produced the current content.
+	*/
+	Header_grypt_version = "Grypt-Version"
+
+	/*
+		Names the encryption and verification scheme used in the ciphertext payload.
+		This header is directly necessary for decryption and verification to work correctly.  Do not change its value.
+	*/
+	Header_grypt_scheme = "Grypt-Scheme"
+
+	/*
+		Names the key used to encrypt the ciphertext payload.
+
+		This is not strictly checked (different users may follow different key naming conventions if they so desire),
+		but by default warnings will be emitted if decryption fails and the keyname does not match the name of the provided key.
+		These warnings are provided to help defuse "D'oh" moments with key management and argument fat-fingering, in the common case where all users of a repo have agreed on the same key names.
+	*/
+	Header_grypt_keyring = "Grypt-Keyring"
+)
+
 type Content struct {
-	Scheme     grypt.Scheme
+	Headers    Headers
 	ciphertext []byte // buffer might turn out more appropriate
 	IV         []byte // probably these should just go as a part of the b64'd ciphertext?
 	MAC        []byte // probably these should just go as a part of the b64'd ciphertext?
@@ -19,10 +60,19 @@ type Content struct {
 
 func (c Content) MarshalBinary() (data []byte, err error) {
 	var buf bytes.Buffer
+	// start header
 	fmt.Fprintf(&buf, "----- BEGIN GRYPT CIPHERTEXT HEADER -----\n")
-	fmt.Fprintf(&buf, "Scheme: %s\n", c.Scheme)
+	// place certain non-optional headers at the front of the parade.  report errors if they are not assigned.
+	// TODO
+	// iterate over remaining headers.  skip the ones already included above.
+	for key, value := range c.Headers {
+		fmt.Fprintf(&buf, "%s: %s\n", key, value)
+	}
+	// end header
 	fmt.Fprintf(&buf, "----- END GRYPT CIPHERTEXT HEADER -----\n")
+	// drop ciphertext.  length is embedded in the binary form.
 	buf.Write(c.ciphertext)
+	// trailing whitespace to moderately decrease the odds of your terminal crying if you cat this file.
 	buf.WriteRune('\n')
 	buf.WriteRune('\n')
 	return buf.Bytes(), nil
@@ -63,8 +113,11 @@ func Encrypt(i io.Reader, o io.Writer, k grypt.Key) error {
 
 	// serialize our header and append the encrypted file
 	// header, err := asn1.Marshal(Header{k.Scheme, iv, hmacMsg.Sum(nil)})
-	header, err := Content{k.Scheme, iv, hmacMsg.Sum(nil), ciphertext.Bytes()}.MarshalBinary()
-	_, err = o.Write(header)
+	headers := Headers{
+		Header_grypt_scheme: fmt.Sprintf("%s", k.Scheme), // TODO this does roughly "what I mean", but should probably be replaced by a marshaller spec on a solid scheme type
+	}
+	serial, err := Content{headers, iv, hmacMsg.Sum(nil), ciphertext.Bytes()}.MarshalBinary()
+	_, err = o.Write(serial)
 	// if err != nil {
 	// 	return err
 	// }
