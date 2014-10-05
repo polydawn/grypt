0caac01157507fbd71567de2b7ae237301e3ed0e
Eric Myhre <hash@exultant.us>
2014-10-05 17:48:36 -0500

    attempt to strap the new encrypter to benchmarking.... this fails bizarrely: go test panics with 'flag redefined: t'; my best guess being because something perverse about renamed package imports is tricking it into running those var initializers twice.  this is not the first time having feature code in the main package has resulted in completely nutzo behavior; we need to doubletime on moving all remaining feature code out of the main package.

--- bench-aes256_sha256_test.go
+++ bench-aes256_sha256_test.go
@@ -1,6 +1,13 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"crypto/rand"
+	"io/ioutil"
+	"testing"
+
+	"polydawn.net/grypt/schema"
+)
 
 func BenchmarkAES256_SHA256Encrypt1K(b *testing.B) {
 	b.ReportAllocs()
@@ -45,4 +52,22 @@ func BenchmarkAES256_SHA256Encrypt4M(b *testing.B) {
 func BenchmarkAES256_SHA256Decrypt4M(b *testing.B) {
 	b.ReportAllocs()
 	decBench(b, AES256_SHA256, 4*(1024*1024))
+}
+
+func BenchmarkNewAES256_SHA256Encrypt1K(b *testing.B) {
+	schem := schema.Aes256sha256ctr{}
+	b.ReportAllocs()
+	k, err := schem.NewKey(rand.Reader)
+	if err != nil {
+		b.Fatal(err)
+	}
+	plaintext := bytes.NewReader(mkRand(1024))
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		if err := schem.Encrypt(plaintext, ioutil.Discard, k); err != nil {
+			b.Fatal(err)
+		}
+		plaintext.Seek(0, 0)
+	}
+	return
 }
