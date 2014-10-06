43fff5c5bae1863222c6d708aea5cc8314a9c8ff
Eric Myhre <hash@exultant.us>
2014-10-05 20:03:36 -0500

    giant mess of work carrying over the function pointer abstractions already used for block cipher and mac system to the stream cipher construction.  still borken, pending resolve of questions about how to make the least bad interfaces.

--- schema/aes256sha256ctr.go
+++ schema/aes256sha256ctr.go
@@ -1,9 +1,9 @@
 package schema
 
 import (
-	"bytes"
+	"crypto/aes"
 	"crypto/cipher"
-	"crypto/hmac"
+	"crypto/sha256"
 	"io"
 )
 
@@ -42,45 +42,5 @@ func (s Aes256sha256ctr) NewKey(entropy io.Reader) (Key, error) {
 	Other headers like which schema this is, etc, are expected to be kept elsewhere as necessary.
 */
 func (s Aes256sha256ctr) Encrypt(input io.Reader, output io.Writer, k Key) error {
-	// Read in the file, calculating the IV and buffering it
-	// impl note: this won't do well with gig files... but this could easily be replaced with re-reading the input, if we had knew we had a resettable reader like a disk
-	plaintext := new(bytes.Buffer)
-	hmacIV := hmac.New(k.Scheme.Hash(), k.hmacKey) // FIXME: parameterize again
-	mw := io.MultiWriter(plaintext, hmacIV)
-
-	if _, err := io.Copy(mw, input); err != nil {
-		return err
-	}
-	iv := hmacIV.Sum(nil)[:s.BlockSize()]
-
-	// commit the iv to output
-	if _, err := output.Write(iv); err != nil {
-		return err
-	}
-
-	// initialize cipher, hmac, and write the expected ciphertext size
-	hmacMsg := hmac.New(k.Scheme.Hash(), k.HMAC) // FIXME: parameterize again
-	blockCipher, err := k.Scheme.NewCipher(k.Key)
-	if err != nil {
-		return err
-	}
-	//TODO the ciphertext size
-
-	// push the input body through the cipher, and the ciphertext both out and through the hmac
-	mw = io.MultiWriter(output, hmacMsg)
-	streamCipher := cipher.StreamWriter{
-		S: cipher.NewCTR(blockCipher, iv),
-		W: mw,
-	}
-	_, err = io.Copy(streamCipher, plaintext)
-	if err != nil {
-		return err
-	}
-	// TODO: verify: the stream cipher had better damn well know how to do padding on close
-
-	// ciphertext body now all pushed; commit the hmac to output and we're done
-	if _, err := output.Write(hmacMsg.Sum(nil)); err != nil {
-		return err
-	}
-	return nil
+	return encrypt(sha256.New, aes.NewCipher, cipher.NewCTR)(input, output, k)
 }
---
+++ schema/crypto.go
@@ -0,0 +1,74 @@
+package schema
+
+import (
+	"bytes"
+	"crypto/cipher"
+	"crypto/hmac"
+	"hash"
+	"io"
+)
+
+type encrypter func(input io.Reader, output io.Writer, k Key) error
+
+/*
+	Generic symmetric encryption for:
+	  - a deterministic content-based IV
+	  - symmetric ciphertext stream (derived from a block cipher via a mode of operation)
+	  - MAC over ciphertext
+	Choice of block cipher, MAC construction, and stream mode are parameters.
+
+	Output is:
+	- IV (fixed length)
+	- length of ciphertext body (fixed length, 8 byte signed long)
+	- ciphertext body
+	- MAC of ciphertext body (fixed length)
+
+	Other headers like which schema this is, etc, are expected to be kept elsewhere as necessary.
+*/
+func encrypt(macFactory func() hash.Hash, cipherFactory func(key []byte) (cipher.Block, error), streamFactory func (cipher.Block, []byte) cipher.Stream) encrypter {
+	// implementation note: the golang stdlib distinction between cipher.Stream and cipher.BlockMode is... odd, and could be readily wallpapered over with some really derpy wrappers.  haven't bothered yet.
+
+	return func(input io.Reader, output io.Writer, k Key) error {
+		// Read in the file, calculating the IV and buffering it
+		// impl note: this won't do well with gig files... but this could easily be replaced with re-reading the input, if we had knew we had a resettable reader like a disk
+		plaintext := new(bytes.Buffer)
+		hmacIV := hmac.New(macFactory, k.hmacKey)
+		mw := io.MultiWriter(plaintext, hmacIV)
+
+		if _, err := io.Copy(mw, input); err != nil {
+			return err
+		}
+		iv := hmacIV.Sum(nil)[:s.BlockSize()]  // WAT for blocksize.  encryptor should closure over this, but is it... argument to encrypt()?  or should all should all encrypt() arguments be boiled down to a schema instance pointer, which can answer all the questions?  this would be less irritating if size() functions had been built into the stdlib crypto interfaces.
+
+		// commit the iv to output
+		if _, err := output.Write(iv); err != nil {
+			return err
+		}
+
+		// initialize cipher, hmac, and write the expected ciphertext size
+		hmacMsg := hmac.New(macFactory, k.hmacKey)
+		blockCipher, err := cipherFactory(k.cipherKey)
+		if err != nil {
+			return err
+		}
+		//TODO the ciphertext size
+
+		// push the input body through the cipher, and the ciphertext both out and through the hmac
+		mw = io.MultiWriter(output, hmacMsg)
+		streamCipher := cipher.StreamWriter{
+			S: streamFactory(blockCipher, iv),
+			W: mw,
+		}
+		_, err = io.Copy(streamCipher, plaintext)
+		if err != nil {
+			return err
+		}
+		// TODO: verify: the stream cipher had better damn well know how to do padding on close
+
+		// ciphertext body now all pushed; commit the hmac to output and we're done
+		if _, err := output.Write(hmacMsg.Sum(nil)); err != nil {
+			return err
+		}
+		return nil
+	}
+}
