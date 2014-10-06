a5bfbb57e4793d91eca28b6659dfae4522d66e3b
Eric Myhre <hash@exultant.us>
2014-10-05 21:44:36 -0500

    finish serializing length headers correctly and doing the decryption implementation.  no tests yet, so assume everything is wrong, but it compiles.

--- schema/aes256sha256ctr.go
+++ schema/aes256sha256ctr.go
@@ -10,7 +10,7 @@ import (
 /*
 	@implements Schema
 */
-type Aes256sha256ctr struct {}
+type Aes256sha256ctr struct{}
 
 func (s Aes256sha256ctr) KeySize() int {
 	return 32
@@ -46,4 +46,9 @@ func (s Aes256sha256ctr) NewKey(entropy io.Reader) (Key, error) {
 */
 func (s Aes256sha256ctr) Encrypt(input io.Reader, output io.Writer, k Key) error {
 	return buildEncrypter(s, sha256.New, aes.NewCipher, cipher.NewCTR)(input, output, k)
+}
+
+func (s Aes256sha256ctr) Decrypt(input io.Reader, output io.Writer, k Key) error {
+	// CTR mode is an interesting degenerate case because it's literally the same stream for encryption and decryption: the stream is just XOR'd.
+	return buildDecrypter(s, sha256.New, aes.NewCipher, cipher.NewCTR)(input, output, k)
 }
--- schema/crypto.go
+++ schema/crypto.go
@@ -4,11 +4,14 @@ import (
 	"bytes"
 	"crypto/cipher"
 	"crypto/hmac"
+	"encoding/binary"
 	"hash"
 	"io"
 )
 
 type encrypter func(input io.Reader, output io.Writer, k Key) error
+
+type decrypter func(input io.Reader, output io.Writer, k Key) error
 
 /*
 	Generic symmetric encryption for:
@@ -19,13 +22,13 @@ type encrypter func(input io.Reader, output io.Writer, k Key) error
 
 	Output is:
 	- IV (fixed length)
-	- length of ciphertext body (fixed length, 8 byte signed long)
+	- length of ciphertext body (fixed length, 8 byte signed big-endian long)
 	- ciphertext body
 	- MAC of ciphertext body (fixed length)
 
 	Other headers like which schema this is, etc, are expected to be kept elsewhere as necessary.
 */
-func buildEncrypter(sch Schema, macFactory func() hash.Hash, cipherFactory func(key []byte) (cipher.Block, error), streamFactory func (cipher.Block, []byte) cipher.Stream) encrypter {
+func buildEncrypter(sch Schema, macFactory func() hash.Hash, cipherFactory func(key []byte) (cipher.Block, error), streamFactory func(cipher.Block, []byte) cipher.Stream) encrypter {
 	// implementation note: the golang stdlib distinction between cipher.Stream and cipher.BlockMode is... odd, and could be readily wallpapered over with some really derpy wrappers.  haven't bothered yet.
 
 	// also: feel slightly bad about passing in both the schema, and all its functors.  could make the Schema interface also return all these things.
@@ -49,13 +52,15 @@ func buildEncrypter(sch Schema, macFactory func() hash.Hash, cipherFactory func(
 			return err
 		}
 
-		// initialize cipher, hmac, and write the expected ciphertext size
+		// initialize cipher, hmac
 		hmacMsg := hmac.New(macFactory, k.hmacKey)
 		blockCipher, err := cipherFactory(k.cipherKey)
 		if err != nil {
 			return err
 		}
-		//TODO the ciphertext size
+
+		// commit the expected ciphertext size to output
+		binary.Write(output, binary.BigEndian, int64(plaintext.Len()%sch.KeySize()+1*sch.KeySize()))
 
 		// push the input body through the cipher, and the ciphertext both out and through the hmac
 		mw = io.MultiWriter(output, hmacMsg)
@@ -67,12 +72,61 @@ func buildEncrypter(sch Schema, macFactory func() hash.Hash, cipherFactory func(
 		if err != nil {
 			return err
 		}
-		// TODO: verify: the stream cipher had better damn well know how to do padding on close
 
 		// ciphertext body now all pushed; commit the hmac to output and we're done
 		if _, err := output.Write(hmacMsg.Sum(nil)); err != nil {
 			return err
 		}
+		return nil
+	}
+}
+
+func buildDecrypter(sch Schema, macFactory func() hash.Hash, cipherFactory func(key []byte) (cipher.Block, error), streamFactory func(cipher.Block, []byte) cipher.Stream) decrypter {
+	// implementation note: the golang stdlib distinction between cipher.Stream and cipher.BlockMode is... odd, and could be readily wallpapered over with some really derpy wrappers.  haven't bothered yet.
+
+	// also: feel slightly bad about passing in both the schema, and all its functors.  could make the Schema interface also return all these things.
+	// really wish the hash/block/stream classes just returned their own sizes consistently, because that would make this a nonissue.
+
+	// @implements decrypter
+	return func(input io.Reader, output io.Writer, k Key) error {
+		// read IV, use it to initialize cipher
+		iv := make([]byte, sch.KeySize())
+		if _, err := io.ReadFull(input, iv); err != nil {
+			return err
+		}
+		blockCipher, err := cipherFactory(k.cipherKey)
+		if err != nil {
+			return err
+		}
+		streamCipher := cipher.StreamWriter{
+			S: streamFactory(blockCipher, iv),
+			W: output,
+		}
+
+		// read length of ciphertext body
+		var bodyLength int64
+		if err := binary.Read(input, binary.BigEndian, &bodyLength); err != nil {
+			return err
+		}
+
+		// read that much into hmac and the decipher stream
+		hmacMsg := hmac.New(macFactory, k.hmacKey)
+		mw := io.MultiWriter(streamCipher, hmacMsg)
+		_, err = io.CopyN(mw, input, bodyLength)
+		if err != nil {
+			return err
+		}
+
+		// read and verify mac
+		mac := make([]byte, sch.MACSize())
+		if _, err := io.ReadFull(input, mac); err != nil {
+			return err
+		}
+		if !hmac.Equal(mac, hmacMsg.Sum(nil)) {
+			return err
+			//return fmt.Errorf("unable to verify file")
+		}
+
 		return nil
 	}
 }
