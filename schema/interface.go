ee0ca994ac249581f3ca2c90ca2bd66154ccfb41
Eric Myhre <hash@exultant.us>
2014-10-09 20:18:58 -0500

    keys are an interface.  or close enough in this case.

--- schema/interface.go
+++ schema/interface.go
@@ -14,3 +14,15 @@ type Schema interface {
 	Encrypt(input io.Reader, output io.Writer, k Key) error
 	Decrypt(input io.Reader, output io.Writer, k Key) error
 }
+
+/*
+	Key struct stores the two byte slices for most symmetric crypto operations:
+	the cipher key and the hmac key.
+
+	This is a simplifying assumption for all the interfaces we currently use, but may break
+	for other kinds of (very) exotic cipher suites we don't yet support.
+*/
+type Key struct {
+	cipherKey []byte
+	hmacKey   []byte
+}
