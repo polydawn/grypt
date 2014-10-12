53851862886be9a6bc3ef57573d31206f9db91fb
Eric Myhre <hash@exultant.us>
2014-10-12 00:52:27 -0500

    test for roundtrip of the full vault format.  fails.

--- vault/vault_test.go
+++ vault/vault_test.go
@@ -1,10 +1,38 @@
 package vault
 
 import (
-	. "github.com/smartystreets/goconvey/convey"
+	"bytes"
+	"crypto/rand"
 	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
+
+	"polydawn.net/grypt/schema"
 )
 
-func TestVaultHeadersRoundtrip(t *testing.T) {
+func TestVaultRoundtrip(t *testing.T) {
+	Convey("Given a cleartext and choice of cipher scheme", t, func() {
+		sch := schema.Aes256sha256ctr{}
+		k, err := sch.NewKey(rand.Reader)
+		So(err, ShouldBeNil)
+		str := "cleartext! :D"
+		cleartext := bytes.NewBuffer([]byte(str))
 
+		Convey("Vault should produce a ciphertext stream", func() {
+			ciphertext := &bytes.Buffer{}
+			WeaveBasket(cleartext, ciphertext, k)
+
+			Convey("Vault should be able to return the cleartext", func() {
+				reheated := &bytes.Buffer{}
+				headers := OpenCiphertext(ciphertext, reheated, k)
+
+				So(headers[Header_grypt_version], ShouldEqual, "1.0")
+				So(headers[Header_grypt_scheme], ShouldEqual, sch.Name())
+				So(headers[Header_grypt_keyring], ShouldEqual, "default")
+
+				So(len(reheated.Bytes()), ShouldEqual, len([]byte(str)))
+				So(string(reheated.Bytes()), ShouldEqual, str)
+			})
+		})
+	})
 }
